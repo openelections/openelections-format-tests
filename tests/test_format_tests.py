11cbc631c661b9311b29128efcfae89ec066d0b6
Kenneth Hsu <kennethhsu@gmail.com>
2021-08-07 15:58:04 -0700

    Add unit test for unknown headers.

--- tests/test_format_tests.py
+++ tests/test_format_tests.py
@@ -30,3 +30,14 @@ class LowercaseHeadersTest(unittest.TestCase):
         format_test = format_tests.LowercaseHeaders()
         format_test.test(["A", "B", "c"])
         self.assertFalse(format_test.passed)
+
+
+class UnknownHeadersTest(unittest.TestCase):
+    def test_headers(self):
+        format_test = format_tests.UnknownHeaders()
+        format_test.test(["a", "b", "c"])
+        self.assertTrue(format_test.passed)
+
+        format_test = format_tests.UnknownHeaders()
+        format_test.test(["a", "UnkNowN", "c"])
+        self.assertFalse(format_test.passed)
