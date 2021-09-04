b9e6f0629a1e7d0f87aa5d0e7757d7ceab41a5a7
Kenneth Hsu <kennethhsu@gmail.com>
2021-09-04 16:34:29 -0700

    Add unit test for test runner script.

--- tests/test_format_tests.py
+++ tests/test_format_tests.py
@@ -1,3 +1,8 @@
+import csv
+import os
+import re
+import subprocess
+import tempfile
 import unittest
 
 from format_tests import format_tests
@@ -157,3 +162,73 @@ class UnknownHeadersTest(unittest.TestCase):
         format_test = format_tests.UnknownHeaders()
         format_test.test(["a", "UnkNowN", "c"])
         self.assertFalse(format_test.passed)
+
+
+class RunTestsTest(unittest.TestCase):
+    bad_data_dir = None
+    bad_rows = [
+        ["County", "unknown", "absentee", "votes", ""],  # Lowercase, unknown, and empty headers
+        ["", "", "", "", ""],  # Empty rows
+        ["a", "b  c", "1", "2", "3"],  # Consecutive whitespace
+        ["a", "b", "c", "1", "2", "3"],  # Inconsistent number of columns
+        ["a", "b", "1", "2.5", "3"],  # Non-integer votes
+        [" a", "b", "1", "2", "3"],  # Leading whitespace
+        ["a ", "b", "1", "2", "3"],  # Trailing whitespace
+        ["a", "b", "1\n2", "2", "3"],  # Premature linebreak
+        ["a", "b", "1\t2", "2", "3"],  # Tab
+        ["a", "b", "1", "2", "3"],  # No errors
+    ]
+    good_data_dir = None
+    good_rows = [
+        ["county", "precinct", "absentee", "votes"],
+        ["a", "b", "1", "2"],
+        ["c", "d", "2", "3"],
+    ]
+    root_path = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir))
+
+    @staticmethod
+    def create_data(root_path, year, rows):
+        year_dir = os.path.join(root_path, year)
+        os.mkdir(year_dir)
+        _, csv_file_path = tempfile.mkstemp(suffix=".csv", dir=year_dir, text=True)
+        with open(csv_file_path, "w") as csv_file:
+            writer = csv.writer(csv_file)
+            writer.writerows(rows)
+
+    @classmethod
+    def setUpClass(cls):
+        cls.bad_data_dir = tempfile.TemporaryDirectory()
+        RunTestsTest.create_data(cls.bad_data_dir.name, "2020", cls.bad_rows)
+
+        cls.good_data_dir = tempfile.TemporaryDirectory()
+        RunTestsTest.create_data(cls.good_data_dir.name, "2020", cls.good_rows)
+
+    def setUp(self):
+        self.log_file = tempfile.NamedTemporaryFile(dir=self.bad_data_dir.name)
+
+    def run_test(self, root_path):
+        command = ["python", os.path.join(RunTestsTest.root_path, "run_tests.py"),
+                   f"--log-file={self.log_file.name}", root_path]
+        completed_process = subprocess.run(command, capture_output=True)
+        return completed_process.returncode
+
+    def test_failure(self):
+        self.assertEqual(1, self.run_test(self.bad_data_dir.name))
+
+        with open(self.log_file.name, "r") as log_file:
+            log_file_contents = "\n".join(log_file.readlines())
+
+        self.assertRegex(log_file_contents, "Header.*" + re.escape(f"{self.bad_rows[0]}") + ".*lowercase")
+        self.assertRegex(log_file_contents, "Header.*" + re.escape(f"{self.bad_rows[0]}") + ".*unknown")
+        self.assertRegex(log_file_contents, "Header.*" + re.escape(f"{self.bad_rows[0]}") + ".*empty")
+        self.assertRegex(log_file_contents, "1 empty rows")
+        self.assertRegex(log_file_contents, "1 rows.*consecutive whitespace")
+        self.assertRegex(log_file_contents, "1 rows.*inconsistent number of columns")
+        self.assertRegex(log_file_contents, "1 rows.*integers")
+        self.assertRegex(log_file_contents, "2 rows.*leading or trailing whitespace")
+        self.assertRegex(log_file_contents, "1 rows.*newline characters")
+        self.assertRegex(log_file_contents, "1 rows.*tab characters")
+        self.assertNotRegex(log_file_contents, re.escape(f"{self.bad_rows[-1]}"))
+
+    def test_success(self):
+        self.assertEqual(0, self.run_test(self.good_data_dir.name))
