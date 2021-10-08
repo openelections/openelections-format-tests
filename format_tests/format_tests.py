938a2938b6f2ebd233f61be8f3f831031cf4bb0d
Kenneth Hsu <kennethhsu@gmail.com>
2021-10-07 17:44:27 -0700

    Fix bug in if condition.
    We actually care if self.__candidate_index is None or not.  Since
    self.candidate_index attains integer values, a value of 0 (i.e.,
    "candidate" is the first column) would previously result in an
    erroneously false value.

--- format_tests/format_tests.py
+++ format_tests/format_tests.py
@@ -252,7 +252,8 @@ class NonIntegerVotes(RowTest):
                 # There are some rare cases where the value represents a turnout percentage.  We will try and avoid
                 # these rows.
                 percentages = {"%", "pct", "percent"}
-                if self.__candidate_index and any(x in row[self.__candidate_index].lower() for x in percentages):
+                if self.__candidate_index is not None \
+                        and any(x in row[self.__candidate_index].lower() for x in percentages):
                     continue
 
                 # If the value isn't numeric, skip the test.  This can be due to the row having an inconsistent
